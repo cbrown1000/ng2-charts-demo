3ebc2b231e00981f1dd6c2512ba3b8d06c8b445f
Chris Brown <cdb186000@gmail.com>
2020-12-19 22:12:13 -0500

    change var name

--- src/app/my-doughnut-chart/my-doughnut-chart.component.ts
+++ src/app/my-doughnut-chart/my-doughnut-chart.component.ts
@@ -9,8 +9,11 @@ export class MyDoughnutChartComponent implements OnInit {
 
   constructor() { }
 
-  public doughnutChartLabels = ['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'];
-  public doughnutChartData = [120, 150, 180, 90];
+  public doughnutChartLabels = ['2006', '2007', '2008', '2009', '2010', '2011', '2012'];//['Sales Q1', 'Sales Q2', 'Sales Q3', 'Sales Q4'];
+  public doughnutChartData = [
+    { data: [65, 59, 80, 81, 56, 55, 40], label: 'Series A' },
+    { data: [28, 48, 40, 19, 86, 27, 90], label: 'Series B' }
+  ];//[120, 150, 180, 90];
   public doughnutChartType = 'doughnut';
 
   ngOnInit() {
